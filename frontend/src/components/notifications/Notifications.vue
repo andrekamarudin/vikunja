41870c6d0bc39bc8ae375f10db100bddad65538c
Andre K <andre.kamarudin@gmail.com>
2026-03-11 01:35:11 +0800

    fix: update NODE_OPTIONS in Dockerfile and correct import type in Notifications.vue

--- frontend/src/components/notifications/Notifications.vue
+++ frontend/src/components/notifications/Notifications.vue
@@ -84,7 +84,7 @@
 
 <script lang="ts" setup>
 import {computed, onMounted, onUnmounted, ref} from 'vue'
-import {useRouter, isNavigationFailure, NavigationFailureType, RouteLocationRaw} from 'vue-router'
+import {useRouter, isNavigationFailure, NavigationFailureType, type RouteLocationRaw} from 'vue-router'
 
 import NotificationService from '@/services/notification'
 import BaseButton from '@/components/base/BaseButton.vue'
